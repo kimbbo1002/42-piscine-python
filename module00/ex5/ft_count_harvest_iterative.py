1b5a67e8ccfc6359c2e397ea2377f96b7ed17b67
Boyoung Kim <bokim@z2r11p7.42lyon.fr>
2026-03-25 17:45:35 +0100

    fix: ex5 iterative range error fixed

--- module00/ex5/ft_count_harvest_iterative.py
+++ module00/ex5/ft_count_harvest_iterative.py
@@ -1,7 +1,7 @@
 def ft_count_harvest_iterative() -> None:
     print("Days until harvest: 5")
-    for i in range(1, 5):
+    for i in range(1, 6):
         print(f"Day {i}")
     print("Harvest time!")
 
-# ft_count_harveest_iterative()
+ft_count_harvest_iterative()
